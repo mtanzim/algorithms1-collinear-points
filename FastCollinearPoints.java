7b9829114e54516606672c4e614b629f9b5cb414
Tanzim Mokammel <tanzim.mokammel@diginex.com>
2019-03-14 18:14:39 +0100

    sort possibly complete, creating line segments

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -30,19 +30,51 @@ public class FastCollinearPoints {
     }
 
     private void generateCollinearFast(Point[] points, int size) {
-        StdOut.println("Fast method!");
         // StdOut.println("Sorted for point: " + i + ": " + points[i].toString());
+        // Points[] orig = points.clone();
         for (int i = 0; i < size; i++) {
-            // for (int j = 0; j < size; j++) {
-            //     if (i == j) continue;
-            if (i > 0) Arrays.sort(points, 0, i, points[i].slopeOrder());
-            if (i < size - 1) Arrays.sort(points, i + 1, size, points[i].slopeOrder());
-            StdOut.println("Sorted for point " + i + ": " + points[i].toString());
-            for (Point point : points) {
-                StdOut.print(point.toString() + " -> ");
+            StdOut.println("i: " + i + ", " + points[i]);
+            // always place current item in the 0th position, and sort
+            Point temp = points[i];
+            points[i] = points[0];
+            points[0] = temp;
+
+            // clone the array
+            Point[] sorted = points.clone();
+            // sort
+            Arrays.sort(sorted, 1, size, temp.slopeOrder());
+
+            // for debug only
+            for (int j = 0; j < size; j++) {
+                StdOut.print(sorted[j].toString() + " -> ");
             }
-            StdOut.println(points);
-            // }
+
+            double prevSlope = temp.slopeTo(sorted[1]);
+            int prevK = 0;
+            int segLenTracker = 0;
+
+            for (int k = 2; k < size; k++) {
+                double newSlope = temp.slopeTo(sorted[k]);
+                if (prevSlope == newSlope) {
+                    segLenTracker++;
+                }
+                else {
+                    if (segLenTracker > 2) {
+                        if (segTracker == segmentArr.length) {
+                            resizeSegmentArr();
+                        }
+                        segmentArr[segmentSize++] = new LineSegment(temp, sorted[k - 1]);
+                    }
+                    segLenTracker = 0;
+                }
+
+
+                prevSlope = newSlope;
+
+            }
+
+
+            StdOut.println("");
         }
 
         // return null;
@@ -96,7 +128,8 @@ public class FastCollinearPoints {
         Point D = new Point(1, 3);
         Point E = new Point(1, 4);
         // Point[] points = { A, B, C, D, E };
-        Point[] points = { C, A, B, E, D };
+        // Point[] points = { C, B, E, A, D };
+        Point[] points = { C, A, E, B, D };
         FastCollinearPoints usainBolt = new FastCollinearPoints(points);
 
 
